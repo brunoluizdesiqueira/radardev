b31a0afcbfbb10562538657f50b64be250940703
Bruno Luiz de Siqueira <brunoluizdesiqueira@gmail.com>
2020-01-17 22:58:05 -0300

    Add project mobile

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://blsdev:sWphOp3UjO7lIk5x@cluster0-tniu4.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://blsdev:cLfvPnY0AXe7ywjS@cluster0-tniu4.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
